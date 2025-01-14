f7ef06c5188c47f4eff18ec5cb546d774a6fcaa2
handongdong <10465128+dongdong9897@user.noreply.gitee.com>
2025-01-14 14:21:47 +0800

    update

--- run.py
+++ run.py
@@ -1,34 +1,41 @@
 import argparse
+from src.predict.predict import inference
 
 parser = argparse.ArgumentParser(description="LLM reasoning and quantitative evaluation ...")
-parser.add_argument("--dataset",default="AGIEval",type=str)
-parser.add_argument("--quantize",default=None,type=str,help="choose an Quantization Algorithm")
-parser.add_argument("--prompt_type",default=None,type=str,help="choose an instruction type")
-parser.add_argument("--chat_mode",default=False,type=bool,help="")
-parser.add_argument("--model_path",default="Qwen2___5-7B-Instruct",type=str,help="choose an LLM model")
-parser.add_argument("--report",default=False,type=bool,help="Generate report or not")
-parser.add_argument("--setting",default='few-shot',type=str,help="choose an reasoning mode ['few-shot','few-shot-CoT','zero-shot','zero-shot-CoT']")
+parser.add_argument("--dataset", default="BBH", type=str)
+parser.add_argument("--quantize", default=None, type=str, help="choose an Quantization Algorithm:[inf8]")
+parser.add_argument("--prompt_type", default=None, type=str, help="choose an instruction type")
+parser.add_argument("--chat_mode", default=False, type=bool, help="")
+parser.add_argument("--model_path", default="Qwen2___5-7B-Instruct", type=str, help="choose an LLM model")
+parser.add_argument("--report", default=False, type=bool, help="Generate report or not")
+parser.add_argument("--batch_size", default=6, type=int, help="")
+parser.add_argument("--setting", default='zero-shot', type=str,
+                    help="choose an reasoning mode ['few-shot','few-shot-CoT','zero-shot','zero-shot-CoT']")
 args = parser.parse_args()
 
 if __name__ == '__main__':
     if args.dataset == "AGIEval":
         from src.processor.AGIEvalProcessor import AGIEval
-        # from src.predict.predict import inference
+
         func = AGIEval(args)
         func.combine_CseRo_prompt()
-        # inference(args)
+        inference(args)
 
     elif args.dataset == "ARC":
         from src.processor.ARCProcessor import ARC
+
         func = ARC(args)
         func.combine_prompt()
+        inference(args)
 
     elif args.dataset == "BBH":
         from src.processor.BBHProcessor import BBH
+
         func = BBH(args)
         func.combine_prompt()
+        inference(args)
 
-    elif args.dataset == "ceval":
-        from src.processor.CevalProcessor import CEVAL
-        func = CEVAL(args)
-        func.combine_prompt()
+    # elif args.dataset == "ceval":
+    #     from src.processor.CevalProcessor import CEVAL
+    #     func = CEVAL(args)
+    #     func.combine_prompt()
--- src/predict/predict.py
+++ src/predict/predict.py
@@ -4,6 +4,7 @@ from src.utils.utils import read_json
 from src.models.ModelInfer import Model
 from conf.model_config import config as configs
 
+
 def prepare(data):
     result = []
     for line in data:
@@ -13,19 +14,19 @@ def prepare(data):
 
 def inference(args):
     if "qwen" in args.model_path.lower():
-        model_path = os.path.join("ckpt",args.model_path)
+        model_path = os.path.join("ckpt", args.model_path)
 
         target_path = "{}-chat".format(args.setting) if args.chat_mode else args.setting
         if args.chat_mode:
             cache_root = "experiments/result/{dataset}/{settings}/{chat_mode}".format(
                 dataset=args.dataset,
-                settings = args.setting,
-                chat_mode = args.chat_mode
+                settings=args.setting,
+                chat_mode=args.chat_mode
             )
         else:
             cache_root = "experiments/result/{dataset}/{settings}".format(
                 dataset=args.dataset,
-                settings = args.setting
+                settings=args.setting
             )
         if not os.path.exists(cache_root) or not os.path.isdir(cache_root):
             # 如果不存在，则创建目录
@@ -35,37 +36,30 @@ def inference(args):
             print(f"Directory '{cache_root}' already exists, skipping creation.")
 
         # 加载参数并初始化模型
-        func = Model(model_path=model_path,config=configs,args=args)
+        func = Model(model_path=model_path, config=configs, args=args)
 
-        
         if args.chat_mode:
             target_file_root = "experiments/cache/{dataset}/{settings}/{chat_mode}".format(
                 dataset=args.dataset,
-                settings = args.setting,
-                chat_mode = args.chat_mode
+                settings=args.setting,
+                chat_mode=args.chat_mode
             )
         else:
             target_file_root = "experiments/cache/{dataset}/{settings}".format(
                 dataset=args.dataset,
-                settings = args.setting
+                settings=args.setting
             )
+        if args.dataset == "AGIEval":
+            target_file_root = "experiments/cache/AGIEval/GseRo"
         file_list = os.listdir(target_file_root)
         print(f"file_list is {file_list}")
         for file in file_list:
-            
-            target_file_path = os.path.join(target_file_root,file)
+            target_file_path = os.path.join(target_file_root, file)
             json_data = read_json(target_file_path)
             cur_data = prepare(json_data)
 
             cur_result = func.infer(cur_data)
-          
-            
-            with open(os.path.join(cache_root,file),"w",encoding="utf-8") as ft:
-                json_data = json.dumps(cur_result,ensure_ascii=False,indent=4)
+
+            with open(os.path.join(cache_root, file), "w", encoding="utf-8") as ft:
+                json_data = json.dumps(cur_result, ensure_ascii=False, indent=4)
                 ft.write(json_data)
-
-
-if __name__ == "__main__":
-    func = QwenModel(
-        model_path="ckpt/Qwen2___5-14B-Instruct"
-    )
