1af3f48d95b0e767656d4e355635fccfd3bed5aa
handongxu <handong_xu@163.com>
2025-02-25 20:49:11 +0800

    update

---
+++ run.sh
@@ -0,0 +1,10 @@
+python run.py --dataset="AGIEval" --setting="GseRo"
+#python run.py --dataset="ARC" --setting="GseRo"
+#python run.py --dataset="BBH" --setting="GseRo"
+#python run.py --dataset="ceval" --setting="GseRo"
+
+#python eval.py --dataset="AGIEval" --setting="GseRo"
+#python eval.py --dataset="ARC" --setting="GseRo"
+#python eval.py --dataset="BBH" --setting="GseRo"
+#python eval.py --dataset="ceval" --setting="GseRo"
+
